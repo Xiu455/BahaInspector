4564bef7347c3e36512bb5a483915c0cc2798ea8
Xiu455 <150805348+Xiu455@users.noreply.github.com>
2025-06-29 20:44:29 +0800

    up

--- src/renderer/src/@cpn/Sidebar/index.jsx
+++ src/renderer/src/@cpn/Sidebar/index.jsx
@@ -3,5 +3,12 @@ import './style.scss'
 export default function Sidebar(){
   return(<div className="sidebar">
     <div className="logo"><img src="./img/telescope.svg" alt="logo" draggable="false" /></div>
+
+    <div className="button-area"></div>
+
+    <button className="setting-btn">
+      <img src="./img/setting.svg" className="icon" draggable="false" />
+      <span>設定</span>
+    </button>
   </div>)
 }
