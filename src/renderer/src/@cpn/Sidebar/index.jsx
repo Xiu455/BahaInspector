1fe10a22d16aaa8ca36b573ccb41010b1e1fd3c3
Xiu455 <150805348+Xiu455@users.noreply.github.com>
2025-06-30 12:07:46 +0800

    up

--- src/renderer/src/@cpn/Sidebar/index.jsx
+++ src/renderer/src/@cpn/Sidebar/index.jsx
@@ -1,13 +1,47 @@
+import { funcPageState } from '../../@util/GlobalValtio'
+
 import './style.scss'
+
+const FuncBtnBox = ({title, children}) => {
+  return(<div className="func-btn-box">
+    <div className="title">{title}</div>
+    <div className="btn-box">
+      {children}
+    </div>
+  </div>)
+}
+
+const BtnS1 = (props) => {
+  const {
+    icon,
+    children,
+    text,
+    ...rest
+  } = props
+
+  return(<button className='btn-S1 fade-in' {...rest}>
+    <div className="icon">{icon ?? ''}</div>
+    <span>{children ?? <p>{text}</p>}</span>
+  </button>)
+}
 
 export default function Sidebar(){
   return(<div className="sidebar">
-    <div className="logo"><img src="./img/telescope.svg" alt="logo" draggable="false" /></div>
+    <div className="button-area">
+      <FuncBtnBox title="功能頁">
+        <BtnS1
+          text='搜尋文章'
+          icon={<i className='bx bx-article'></i> }
+        />
+      </FuncBtnBox>
+    </div>
 
-    <div className="button-area"></div>
+    <div className="sep-w"></div>
 
-    <button className="setting-btn">
-      <img src="./img/setting.svg" className="icon" draggable="false" />
+    <button
+      className="setting-btn fade-in"
+    >
+      <div className="icon"><i className='bxr bx-cog'></i></div>
       <span>設定</span>
     </button>
   </div>)
